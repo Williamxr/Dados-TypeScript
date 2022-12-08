9eef6f95b12b8be7bfb5aa7e1e08135950f548fd
William <william.teletabs@hotmail.com>
2022-12-07 21:37:54 -0300

    Criando função para puxar api e retornando em arquivo json

---
+++ src/script.ts
@@ -0,0 +1,8 @@
+import fetchData from "./fetchData.js";
+
+async function handleData() {
+  const data =  await fetchData("https://api.origamid.dev/json/transacoes.json");
+  console.log(data);
+}
+
+handleData();
