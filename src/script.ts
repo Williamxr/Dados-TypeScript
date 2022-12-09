8c055b3dd3f53585a476ad6683e3d3c687e5ba09
William <william.teletabs@hotmail.com>
2022-12-09 10:57:47 -0300

    Importando normalizarTransacao

--- src/script.ts
+++ src/script.ts
@@ -1,30 +1,12 @@
 import fetchData from "./fetchData.js";
-
-type TransacaoPagamento = "Boleto" | "Cartão de Crédito";
-type TransacaoStatus = "Paga" 
-| "Recusada pela operadora de cartão" 
-| "Aguaradndo pagamento" 
-| "Estornada";
-
-interface TransacaoAPI{
-  Nome: string;
-  ID: number;
-  Data: string;
-  Status: TransacaoStatus;
-  Email: string;
-  ['Valor ()']: string;
-  ['Forma de Pagamento ()']: TransacaoPagamento;
-  ['Cliente novo ()']: number;
-}
+import normalizarTransacao from "./normalizarTransacao.js";
 
 async function handleData() {
   const data =  await fetchData<TransacaoAPI[]>("https://api.origamid.dev/json/transacoes.json");
 
-  if(data){
-    data.forEach(item => {
-      console.log(item.Email)
-    });
+  if(!data) return;
+    const transacoes = data.map(normalizarTransacao);
+    console.log(transacoes);
   }
-}
 
 handleData();
