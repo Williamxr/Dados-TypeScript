279a0596f7d0bc341697407bf55a5ae470968131
William <william.teletabs@hotmail.com>
2022-12-14 14:23:12 -0300

    Refatorando código

--- src/script.ts
+++ src/script.ts
@@ -1,36 +1,47 @@
-import Estatisticas from "./Estatisticas.js";
+import { CountList } from "./countBy.js";
+import Estatiscas from "./Estatisticas.js";
 import fetchData from "./fetchData.js";
 import normalizarTransacao from "./normalizarTransacao.js";
 
 async function handleData() {
-  const data =  await fetchData<TransacaoAPI[]>("https://api.origamid.dev/json/transacoes.json");
+  const data = await fetchData<TransacaoAPI[]>(
+    "https://api.origamid.dev/json/transacoes.json?"
+  );
+  if (!data) return;
+  const transacoes = data.map(normalizarTransacao);
+  preencherTabela(transacoes);
+  preencherEstatisticas(transacoes);
+}
 
-  if(!data) return;
-    const transacoes = data.map(normalizarTransacao);
-    preencherTabela(transacoes);
-    preencherEstatisticas(transacoes);
+function preencherLista(lista: CountList, containerId: string): void {
+  const containerElement = document.getElementById(containerId);
+  if (containerElement) {
+    Object.keys(lista).forEach((key) => {
+      containerElement.innerHTML += `<p>${key}: ${lista[key]}</p>`;
+    });
   }
+}
 
-  function preencherEstatisticas(transacoes: Transacao[]):void{
-    const data = new Estatisticas(transacoes);
+function preencherEstatisticas(transacoes: Transacao[]): void {
+  const data = new Estatiscas(transacoes);
 
-    console.log(data);
+  preencherLista(data.pagamento, "pagamento");
+  preencherLista(data.status, "status");
 
-    const totalElement = document.querySelector<HTMLElement>('#total span');
-    if(totalElement) {
-      totalElement.innerText = data.total.toLocaleString("pt-BR", {
-        style: "currency",
-        currency: "BRL",
-      });
-    }
+  const totalElement = document.querySelector<HTMLElement>("#total span");
+  if (totalElement) {
+    totalElement.innerText = data.total.toLocaleString("pt-BR", {
+      style: "currency",
+      currency: "BRL",
+    });
   }
+}
 
-  function preencherTabela(transacoes: Transacao[]): void{
-    const tabela = document.querySelector("#transacoes tbody");
-    if(!tabela) return;
-
-    transacoes.forEach((transacao) => {
-      tabela.innerHTML += `
+function preencherTabela(transacoes: Transacao[]): void {
+  const tabela = document.querySelector("#transacoes tbody");
+  if (!tabela) return;
+  transacoes.forEach((transacao) => {
+    tabela.innerHTML += `
       <tr>
         <td>${transacao.nome}</td>
         <td>${transacao.email}</td>
@@ -38,8 +49,8 @@ async function handleData() {
         <td>${transacao.pagamento}</td>
         <td>${transacao.status}</td>
       </tr>
-      `
-    });
-  }
+    `;
+  });
+}
 
 handleData();
